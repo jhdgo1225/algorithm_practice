b7119c32b91f87c1b88e514648e40a95044f76f9
jhdgo1225 <parkjh99365@gmail.com>
2022-02-22 00:02:56 +0900

    Review dfs and bfs

--- dfs_bfs/bfs.py
+++ dfs_bfs/bfs.py
@@ -10,7 +10,7 @@ def bfs(graph, start, visited):
     while queue:
         # 큐에서 하나의 원소를 뽑아 출력
         v = queue.popleft()
-        print(v, end=' ')
+        print(v, end=" ")
         # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
         for i in graph[v]:
             if not visited[i]:
