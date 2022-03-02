a3980e03aa830b58744ec49f0d5c54f7531d7e5a
jhdgo1225 <parkjh99365@gmail.com>
2022-03-02 21:55:35 +0900

    Review DFS/BFS

--- dfs_bfs/recursive.py
+++ dfs_bfs/recursive.py
@@ -1,9 +1,13 @@
-def recursive_function(i):
-    # 100번째 출력했을 때 종료되도록 종료 조건 명시
-    if i == 100:
-        return
-    print(f"{i} 번째 재귀 함수에서 {i+1} 번째 재귀 함수를 호출합니다.")
-    recursive_function(i+1)
-    print(f"{i} 번째 재귀 함수를 종료합니다.")
+def factorial_iterative(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
 
-recursive_function(1)
+def factorial_recursive(n):
+    if n <= 1:
+        return 1
+    return n * factorial_recursive(n-1)
+
+print('반복적으로 구현:', factorial_iterative(5))
+print('재귀적으로 표현:', factorial_recursive(5))
